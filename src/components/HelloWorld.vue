df589b714a8cb53c062eb49910e2a15ef8e1c9b1
GwanHooPark <hooho.dev@gmail.com>
2020-05-04 19:30:20 +0900

    pwa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,30 +25,34 @@ export default {
       console.log('before install')
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
+      window.deferredPrompt = e;
       // Stash the event so it can be triggered later.
       // eslint-disable-next-line no-undef
 
-      let deferredPrompt = e;
+      /*var deferredPrompt = e;
       // Update UI to notify the user they can add to home screen
       let addBtn = document.querySelector(".add-button");
-      addBtn.style.display = 'block';
+      addBtn.style.display = 'block';*/
 
-      addBtn.addEventListener('click', (e) => {
-        console.log(e)
+      //addBtn.addEventListener('click', (e) => {
+
         // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
+        //addBtn.style.display = 'none';
         // Show the prompt
-        deferredPrompt.prompt();
+      window.deferredPrompt.prompt();
         // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
+      window.deferredPrompt.userChoice.then((choiceResult) => {
           if (choiceResult.outcome === 'accepted') {
             console.log('User accepted the A2HS prompt');
           } else {
             console.log('User dismissed the A2HS prompt');
           }
-          deferredPrompt = null;
+        window.deferredPrompt = null;
         });
-      });
+      //});
+    });
+    window.addEventListener('appinstalled', () => {
+      console.log("app installed");
     });
   }
 };
